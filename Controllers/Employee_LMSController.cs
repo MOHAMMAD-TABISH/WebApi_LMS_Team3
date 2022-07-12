2a154610503964d2bd625bab5bc9774af6f68929
Mohammad Tabish <1000071044@hexaware.com>
2022-07-12 21:11:28 +0530

    ok

--- Controllers/Employee_LMSController.cs
+++ Controllers/Employee_LMSController.cs
@@ -24,5 +24,14 @@ namespace WebApi_LMS_Team3.Controllers
             var ar = _dataAccessLayer_LMS.Employee_T.ToList();
             return ar;
         }
+        [HttpPost]
+        public ActionResult<List<EmployeeDb>> Insert(EmployeeDb EmpModel)
+        {
+            var ar = _dataAccessLayer_LMS.Employee_T.Add(new EmployeeDb { Emp_Name = EmpModel.Emp_Name, Emp_City = EmpModel.Emp_City, Emp_Dept = EmpModel.Emp_Dept, Emp_DOB = EmpModel.Emp_DOB, Emp_Email = EmpModel.Emp_Email, Emp_Gender = EmpModel.Emp_Gender, Emp_Id = EmpModel.Emp_Id, Emp_Mobile = EmpModel.Emp_Mobile, Emp_Salary = EmpModel.Emp_Salary });
+            _dataAccessLayer_LMS.SaveChanges();
+            var res = _dataAccessLayer_LMS.Employee_T.ToList();
+            return res;
+        }
+
     }
 }
