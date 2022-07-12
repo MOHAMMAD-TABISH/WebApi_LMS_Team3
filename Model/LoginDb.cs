a44ead39aaae27defe120573d5a97678f8168b60
Mankala Soumya <1000071098@hexaware.com>
2022-07-12 20:50:43 +0530

    Leave

---
+++ Model/LoginDb.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi_LMS_Team3.Model
+{
+    public class LoginDb
+    {
+        [Key]
+        public int Emp_Id { get; set; }
+        public string Emp_Password { get; set; }
+        public string SecurityQuestion { get; set; }
+        public int SecurityAnswer { get; set; }
+    }
+}
