add680db9134df8250e781be4e8431eb1988fb05
Mohammad Tabish <1000071044@hexaware.com>
2022-07-13 17:20:43 +0530

    Con

--- Model/EmpLeaveStatusDb.cs
+++ Model/EmpLeaveStatusDb.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApi_LMS_Team3.Model
 {
     public class EmpLeaveStatusDb
     {
+        [Key]
         public int Leave_Id { get; set; }
         public int NoOfDays { get; set; }
         public DateTime StartDate { get; set; }
