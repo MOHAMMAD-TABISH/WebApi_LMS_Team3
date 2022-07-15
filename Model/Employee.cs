789053cbcfbd82e24626cd8eff85a3875e1c77f1
muskankesarwani08 <muskankesarwani08@gmail.com>
2022-07-15 13:14:55 +0530

    Data Annotation

--- Model/Employee.cs
+++ Model/Employee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,10 +9,15 @@ namespace WebApi_LMS_Team3.Model
     public class Employee
     {
         public int Emp_Id { get; set; }
+        [Required]
         public string Emp_Name { get; set; }
+        [Required]
         public string Emp_Email { get; set; }
+        [Required]
         public long Emp_Mobile { get; set; }
+        [Required]
         public string Emp_Dept { get; set; }
+        [Required]
         public long Available_Leave { get; set; }
     }
 }
---
+++ Model/Manager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi_LMS_Team3.Model
+{
+    public class Manager
+    {
+        public int Mng_Id { get; set; }
+        [Required]
+        public string Mng_Name { get; set; }
+        [Required]
+        public string Mng_Email { get; set; }
+        [Required]
+        public long Mng_Mobile { get; set; }
+    }
+}
