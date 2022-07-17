1210bafb4b1b36af598df244b47454500fe3c16e
muskankesarwani08 <muskankesarwani08@gmail.com>
2022-07-17 12:38:45 +0530

    Leave Controller

--- Controllers/ApplyLeaveController.cs
+++ Controllers/ApplyLeaveController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApi_LMS_Team3.Model;
 
 namespace WebApi_LMS_Team3.Controllers
 {
@@ -11,5 +12,36 @@ namespace WebApi_LMS_Team3.Controllers
     [ApiController]
     public class ApplyLeaveController : ControllerBase
     {
+        private readonly IApplyLeaveRepo applyLeaveRepo;
+
+        public ApplyLeaveController(IApplyLeaveRepo applyLeaveRepo)
+        {
+            this.applyLeaveRepo = applyLeaveRepo;
+
+        }
+        [HttpGet]
+        [Route("ShowAll_Leaves/{id}")]
+        public List<ApplyLeave> Leave_List(int? id)
+        {
+            var list = applyLeaveRepo.ShowAllLeaves(id);
+            return list;
+        }
+        [HttpPatch]
+        [Route("ChangeStatus/{id}")]
+
+        public int ChangeState(int? id, ApplyLeave applyLeave)
+        {
+            var data = applyLeaveRepo.ManagerState(id, applyLeave);
+            return 1;
+        }
+        [HttpPut]
+        [Route("NewLeave")]
+
+        public async Task<int> AddNewLeave(ApplyLeave leave)
+        {
+            var newleave = await applyLeaveRepo.NewLeave(leave);
+            return 1;
+        }
+
     }
 }
