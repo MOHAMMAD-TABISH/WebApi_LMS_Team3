19d2cae13ec7f754420bb8d27eaea172c2ae64e6
Mohammad Tabish <1000071044@hexaware.com>
2022-07-17 12:24:29 +0530

    Employee Repository

---
+++ Repository/EmployeeRepo.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi_LMS_Team3.DataAccessLayer;
+using WebApi_LMS_Team3.Model;
+
+namespace WebApi_LMS_Team3.Repository
+{
+    public class EmployeeRepo
+    {
+        private readonly DataAccessLayer_LMS dataAccessLayer_LMS;
+        private readonly IMapper mapper;
+
+        public EmployeeRepo(DataAccessLayer_LMS dataAccessLayer_LMS, IMapper mapper)
+        {
+            this.dataAccessLayer_LMS = dataAccessLayer_LMS;
+            this.mapper = mapper;
+        }
+
+
+
+        public async Task<int> Insert_Employee_Async(Employee employee)
+        {
+            var add = mapper.Map<EmployeeDb>(employee);
+
+            if (add != null)
+            {
+                await dataAccessLayer_LMS.Employee_T.AddAsync(add);
+                await dataAccessLayer_LMS.SaveChangesAsync();
+
+            }
+
+            return 1;
+        }
+
+        public async Task<Employee> MyDetailsAsync(int? id)
+        {
+            var details = await dataAccessLayer_LMS.Employee_T.FirstOrDefaultAsync(x => x.Emp_Id == id);
+            var details_View = mapper.Map<Employee>(details);
+
+            return details_View;
+        }
+
+        public async Task<List<Employee>> ShowAllEMPAsync()
+        {
+            var list_emp = await dataAccessLayer_LMS.Employee_T.ToListAsync();
+            var map_list = mapper.Map<List<Employee>>(list_emp);
+            return map_list;
+        }
+    }
+}
--- Repository/IEmployeeRepo.cs
+++ Repository/IEmployeeRepo.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApi_LMS_Team3.Model;
 
 namespace WebApi_LMS_Team3.Repository
 {
-    public class IEmployeeRepo
+    interface IEmployeeRepo
     {
+        Task<Employee> MyDetailsAsync(int? id);
+        Task<List<Employee>> ShowAllEMPAsync();
+        Task<int> Insert_Employee_Async(Employee employee);
     }
 }
